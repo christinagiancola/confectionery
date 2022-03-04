26bc58441c9be7407f2fa0bd6e60119ef88ec4d6
Christina Giancola <christinamgiancola@gmail.com>
2022-03-04 17:25:57 -0500

    WIP react-slick carousel in <CardStack>

--- src/components/CardStack.js
+++ src/components/CardStack.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useRef } from "react"
+import React, { useState } from "react"
 import Slider from "react-slick"
 // import { FaArrowRight, FaArrowLeft } from "react-icons/fa"
 
 const CardStack = ({ cards, currentClickId }) => {
   console.log(cards)
-  const imageUrls = cards.map(card => card.file.url)
+  const imgUrls = cards.map(card => card.file.url)
+  const [imgIndex, setImgIndex] = useState(0)
   const contactCardIndex = cards.length - 1
+  const infoCardIndex = 0
   let cardArray = [
     <img src="https://images.ctfassets.net/jotoby554kx0/4bhIAoUyQeYysPWgIe5SCK/4c223a881085f01d75dd5c440bf6fde1/CARD01.jpg"></img>,
     <img src="https://images.ctfassets.net/jotoby554kx0/3wzwAUSG70EhPvHVphmKJw/e161255869e20cc89e8dd2aba42d1206/CARD02.jpg"></img>,
@@ -14,7 +16,7 @@ const CardStack = ({ cards, currentClickId }) => {
     <img src="https://images.ctfassets.net/jotoby554kx0/4GExflYYHixT6c0bgnu4OI/23d950c158ec15d0a2f49e660b8ed51b/CARD_05.jpg"></img>,
   ]
 
-  console.log("<CardStack> imageUrls:", imageUrls)
+  console.log("<CardStack> imageUrls:", imgUrls)
   console.log("<CardStack> cardArray:", cardArray)
 
   const settings = {
@@ -27,36 +29,35 @@ const CardStack = ({ cards, currentClickId }) => {
     centerMode: true,
     centerPadding: 0,
     draggable: true,
+    swipeToSlide: true,
     initialSlide: 0,
     slidesToShow: 3,
   }
 
-  // const goNext = () => {
-  //   if (ref.current !== null && ref.current.swiper !== null) {
-  //     ref.current.swiper.slideNext()
-  //   }
+  // const NextArrow = ({ onClick }) => {
+  //   return (
+  //     <div className="arrow next" onClick={onClick}>
+  //       <FaArrowRight />
+  //     </div>
+  //   )
   // }
 
-  // const goPrev = () => {
-  //   if (ref.current !== null && ref.current.swiper !== null) {
-  //     ref.current.swiper.slidePrev()
-  //   }
+  // const PrevArrow = ({ onClick }) => {
+  //   return (
+  //     <div className="arrow prev" onClick={onClick}>
+  //       <FaArrowLeft />
+  //     </div>
+  //   )
   // }
-
-  // useEffect(() => {
-  //   imageUrls.map((url, index) => {
-  //     cardArray.push(<img key={index} src={url} alt=""></img>)
-  //   })
-  // }, [imageUrls])
 
   console.log("CardStack")
 
   return (
     <div className="container">
       <Slider {...settings}>
-        {imageUrls.map((img, idx) => (
-          <div>
-            <img src={img} alt={idx} />
+        {imgUrls.map((img, index) => (
+          <div className={index === imgIndex ? "slide activeSlide" : "slide"}>
+            <img src={img} alt={index} />
           </div>
         ))}
       </Slider>
